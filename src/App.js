63f7a348772e7c88a438aa7b6b23c3b08efea578
Jared Fitts <jfitts@gmail.com>
2021-03-18 19:59:58 -0600

    created app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react' ;
+import GroceryList from './components/groceries/GroceryList';
+import GroceryForm from './components/groceries/GroceryForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { grocerys : [
+{ id:1, title: "apples", complete: false },
+{ id:2, title: "oranges", complete: false },
+{ id:3, title: "meat", complete: false },
+   ]
+  }
+
+   getUniqId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+   addGrocery = (incommingGrocery) => {
+     const { grocerys } =this.state
+     const { title, complete } = incommingGrocery
+     const newGrocery = { id: this.getUniqId(), title, complete }
+     this.setState({ grocerys :[newGrocery, ...grocerys] })
+   }
+
+   updateComplete = (id) => {
+    const { grocerys } = this.state
+    this.setState({
+      grocerys: grocerys.map( t => {
+        if (t.id === id) {
+          return {
+            ...t, 
+            complete: !t.complete
+          }
+        }
+
+
+        return t
+      })
+    })
+  }
+    render() {
+      const { grocerys } = this.state
+      return (
+        <>
+          <GroceryList grocerys={grocerys} updateComplete={this.updateComplete} />
+          <GroceryForm addGrocery={this.addGrocery} />
+        </>
+      )
+  }
+
 }
+
+
 
 export default App;
